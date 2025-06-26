bd422ceac190909de0ae382641cda21caf9214f4
Gabriel Schirrmann Faria <gabrielschirrmannfarias@gmail.com>
2025-06-25 23:09:19 -0400

    ultima atualização

--- OS/chamados/views.py
+++ OS/chamados/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import OrdemServico
+from .forms import OrdemServicoForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def index(request):
+    if request.method == 'POST':
+        form = OrdemServicoForm(request.POST)
+        if form.is_valid():
+            nova_os = form.save(commit=False)
+            nova_os.status = 'aberto'
+            nova_os.save()
+            return redirect('index')
+    else:
+        form = OrdemServicoForm()
+
+    ordens = OrdemServico.objects.all()
+    return render(request, 'index.html', {'form': form, 'ordens': ordens})
